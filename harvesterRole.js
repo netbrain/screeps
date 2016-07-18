d1c9a91800626df31a7fb087085177e6e5ea14a0
Kim Eik <kim@heldig.org>
2016-07-18 23:06:20 +0200

    ugh

--- harvesterRole.js
+++ harvesterRole.js
@@ -1,5 +1,20 @@
 module.exports = {
   run: function(creep){
+        var target = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+        var storage = creep.pos.findClosestByRange(FIND_MY_SPAWNS);
+        var total = _.sum(creep.carry);
+
+        if(total >= creep.carryCapacity){
+            for(var resourceType in creep.carry) {
+            	if(creep.transfer(storage, resourceType) === ERR_NOT_IN_RANGE){
+            	    creep.moveTo(storage);
+            	}
+            }
+        }else if(target) {
+            if(creep.harvest(target) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
+        }
 
   }
 };
--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 var _ = require("lodash");
 var harvesterRole = require("harvesterRole");
+
+var WORKER_CREEP = [WORK,MOVE,CARRY];
+var FAST_WORKER_CREEP = [WORK,MOVE,MOVE,CARRY];
+
+
+_.each(Game.spawns, function(spawn){
+    if(spawn.canCreateCreep(FAST_WORKER_CREEP) === OK){
+        spawn.createCreep(FAST_WORKER_CREEP);
+        console.log("Creating fast worker");
+    }else if(spawn.canCreateCreep(WORKER_CREEP) === OK){
+        spawn.createCreep(WORKER_CREEP);
+        console.log("Creating normal worker");
+    }
+})
+
+_.each(Game.creeps, function(creep){
+  harvesterRole.run(creep);
+});
